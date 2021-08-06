89ee061bc271732cd951f17e704cdecaf54b1b46
Nick Magidson <55424886+NickMagidson@users.noreply.github.com>
2021-08-05 20:55:09 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return <div> 
+    <h3>Hello World!</h3>
+    <h4>Goodbye</h4>
+    <Folder/>
+    </div> 
+}
+
+const Folder = () => {
+  return <h4>Folder</h4>
+};
+
+
+export default App;
