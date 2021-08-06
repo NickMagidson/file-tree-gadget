efa1a0608bead0f40cc25b85d0d5c96d90431728
Nick Magidson <55424886+NickMagidson@users.noreply.github.com>
2021-08-05 21:27:06 -0400

    Basic building

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import React from "react";
 
 function App() {
   return <div> 
-    <Folder name="Desktop"/>
+    <Folder name="Desktop" isOpen={false}>
+      <Folder name="Music">
+        <File name="all_star.mp3"/>
+        <File name="express_file.mp3"/>  
+      </Folder>
+      <File name="dogs.jpeg"/>
+      <File name="cats.png"/>
+    </Folder>
+
     <Folder name="Applications"/>
+
     </div> 
 }
 
 const Folder = (props) => {
-  return <h4>{props.name}</h4>
+  return <div>
+    {props.name}
+    <div style={{ marginLeft: '17px' }}>
+    {props.isOpen ? props.children: null}
+    </div>
+  </div>
 };
 
 const File = (props) => {
-  
+  return <div>{props.name}</div>
 }
 
 
